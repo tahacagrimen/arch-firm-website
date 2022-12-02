d1bdd6ac2d24017443ed605e296495166eb03dc8
tahacagrimen <tahacagrimen2@gmail.com>
2022-12-02 16:15:43 +0300

    1

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const location = useLocation();
+
+  const { pathname } = location;
+
+  console.log(pathname);
+
+  const handleHangdownText = (path: string): string => {
+    if (path === "/") {
+      return "ANASAYFA";
+    } else if (path === "/about") {
+      return "HAKKIMDA";
+    } else if (path === "/contact") {
+      return "İLETİŞİM";
+    } else if (path === "/projects") {
+      return "PROJELER";
+    } else if 
+
+  };
+
+  return (
+    <div>
+      <div className="tersyazi">
+        <div className="tersyazi__cizgi"></div>
+        <h3>{handleHangdownText(pathname)}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
